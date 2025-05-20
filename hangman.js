7b92906541564520ebf8437e81223986ccafc2e1
Menke <25MenkeL@badinhs.org>
2025-05-20 16:11:06 -0400

    made the word hidden / replaced the letters with underscores

--- hangman.js
+++ hangman.js
@@ -1,1 +1,12 @@
 let wordBank = ["Stevie T Badin", "Javascript", "Cincinnati", "Ohio State", "Reds Country"];
+
+let secret = wordBank[Math.floor(Math.random() * wordBank.length)]. toUpperCase();
+
+let guessed = [];
+for (let i = 0; i < secret.length; i++) {
+    if (secret[i] === " ") {
+        guessed.push(" ");
+    } else {
+        guessed.push("_");
+    }
+}
